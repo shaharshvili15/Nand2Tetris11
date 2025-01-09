56b3d6968746371d889808b8099222ac87484201
Eytan Spialter <eytan.spialter@post.runi.ac.il>
2025-01-09 15:21:54 +0200

    implemented main to work with JackAnalyzer given by the staff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length != 1) {
+            System.out.println("Usage: java Main <file.jack or directory>");
+            return;
+        }
+
+        // Pass the argument to JackAnalyzer
+        JackAnalyzer.main(args);
     }
 }
