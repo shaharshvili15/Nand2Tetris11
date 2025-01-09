e74bf5ad14fc54d3786c2b786f0d5c46149cf8b2
Eytan Spialter <eytan.spialter@post.runi.ac.il>
2025-01-09 17:38:35 +0200

    fixed Main to work from terminal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        //todo : change before submission
-        // if (args.length != 1) {
-        //     System.out.println("Usage: java Main <file.jack or directory>");
-        //     return;
-        // }
-        JackAnalyzer.main(new String[]{"/Users/eytanspialter/Desktop/Code/Digital_Systems/project10/submition/10/10ArrayTest/Main.jack"});
+        if (args.length != 1) {
+            System.out.println("Usage: java Main <file.jack or directory>");
+            return;
+        }
         // Pass the argument to JackAnalyzer
-        //todo : change before submission
-        //JackAnalyzer.main(args);
+        JackAnalyzer.main(args);
     }
 }
